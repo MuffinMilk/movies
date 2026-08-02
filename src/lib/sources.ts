65fcc78d1bba83176a56b492ee933062a84dca82
MuffinMilk <Awdrej.puente@icloud.com>
2026-08-01 23:54:25 -0500

    chore: update and expand movie streaming sources
    Replace deprecated providers with current alternatives to ensure reliable content streaming.

--- src/lib/sources.ts
+++ src/lib/sources.ts
@@ -7,9 +7,9 @@ export interface Source {
 }
 
 export const sources: Source[] = [
-  { id: 'vidsrcin', name: 'VidSrc IN', isFrench: false, url: 'https://vidsrc.in/embed/movie/{id}', tvUrl: 'https://vidsrc.in/embed/tv/{id}/{season}/{episode}' },
-  { id: 'vidsrcpm', name: 'VidSrc PM', isFrench: false, url: 'https://vidsrc.pm/embed/movie/{id}', tvUrl: 'https://vidsrc.pm/embed/tv/{id}/{season}/{episode}' },
-  { id: 'embedsu', name: 'EmbedSU', isFrench: false, url: 'https://embed.su/embed/movie/{id}', tvUrl: 'https://embed.su/embed/tv/{id}/{season}/{episode}' },
-  { id: 'vidsrcxyz', name: 'VidSrc XYZ', isFrench: false, url: 'https://vidsrc.xyz/embed/movie?tmdb={id}', tvUrl: 'https://vidsrc.xyz/embed/tv?tmdb={id}&season={season}&episode={episode}' },
+  { id: 'vidsrccc', name: 'VidSrc CC', isFrench: false, url: 'https://vidsrc.cc/v2/embed/movie/{id}', tvUrl: 'https://vidsrc.cc/v2/embed/tv/{id}/{season}/{episode}' },
+  { id: 'vidlink', name: 'VidLink', isFrench: false, url: 'https://vidlink.pro/movie/{id}', tvUrl: 'https://vidlink.pro/tv/{id}/{season}/{episode}' },
+  { id: 'vidsrcme', name: 'VidSrc ME', isFrench: false, url: 'https://vidsrc.me/embed/movie?tmdb={id}', tvUrl: 'https://vidsrc.me/embed/tv?tmdb={id}&season={season}&episode={episode}' },
+  { id: '2embed', name: '2Embed', isFrench: false, url: 'https://www.2embed.cc/embed/{id}', tvUrl: 'https://www.2embed.cc/embedtv/{id}&s={season}&e={episode}' },
   { id: 'jellyfin', name: 'Custom Jellyfin / Local Server', isFrench: false, url: '{customUrl}', tvUrl: '{customUrl}' }
 ];
